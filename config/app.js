5c6dd84e06db9cafa08404ded3a17b510ce553ba
yafu <zhyaphoo@qq.com>
2026-09-11 16:39:42 +0800

    设置静态资源缓存时间10天

--- config/app.js
+++ config/app.js
@@ -5,7 +5,12 @@
 const app = {
     app_debug: true, //调试模式
     default_deep: 'app', //默认应用深度
-    static_dir: './public', //静态文件目录，相对于应用根目录，为空或false时，关闭静态访问
+    static_dir: {
+        static_dir: './public', //静态文件目录，相对于应用根目录，为空或false时，关闭静态访问
+        options: {
+            maxage: 10 * 24 * 60 * 60 * 1000, //静态文件缓存时间，单位毫秒
+        }
+    },
     koa_body: {
         multipart: true,
         // 设置请求体大小限制
